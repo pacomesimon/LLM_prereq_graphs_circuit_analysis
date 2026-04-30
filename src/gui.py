6727083c504ad3f119388f13594fbfa21ca2d1a4
pacomesimon <smbonimpa2011@gmail.com>
2026-04-30 13:22:42 +0200

    testing_assets

--- src/gui.py
+++ src/gui.py
@@ -194,7 +194,7 @@ def create_ui():
                         gr.Markdown("---")
                         upload_eval_report = gr.File(label="Upload Existing Eval Report (JSON)", file_types=[".json"])
                         gr.Examples(
-                            examples=[["assets/full_eval_report_2u970ry9.json"]],
+                            examples=[["assets/full_dataset_6hxx81y3.json"]],
                             inputs=upload_eval_report,
                             label="Eval Report Example"
                         )
